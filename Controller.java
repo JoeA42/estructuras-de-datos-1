fb65439616f0cf77f983b0236bdcd461ae74c1b3
JoeA42 <36778393+JoeA42@users.noreply.github.com>
2020-02-25 22:39:59 -0600

    Updated chat functionality
    the chat window now prints the text message that is entered in the chat field.

--- Controller.java
+++ Controller.java
@@ -1,13 +1,23 @@
 package sample;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.*;
 
 
 public class Controller {
+
+    public TextArea chatWindow;
+    public TextField textInput;
+    public MenuItem ipConnection;
+
     public void pressButton(ActionEvent event){
-        System.out.println("1salu2T");
+        String user = "User: ";
+        String text = textInput.getText();
+        textInput.clear();
+        chatWindow.appendText(user + text + "\n");
 
 
     }
-}
+    }
+
 
