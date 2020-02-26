fbfc3fcc290c379602d3f169d32d571c50fa1ace
JoeA42 <36778393+JoeA42@users.noreply.github.com>
2020-02-26 00:02:01 -0600

    Added functionality to menu items
    When you click a menu item, a second stage opens, and display's a new window.

--- Controller.java
+++ Controller.java
@@ -1,7 +1,17 @@
 package sample;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import javax.swing.*;
+import java.io.IOException;
+
+import static javafx.fxml.FXMLLoader.*;
 
 
 public class Controller {
@@ -9,15 +19,33 @@ public class Controller {
     public TextArea chatWindow;
     public TextField textInput;
     public MenuItem ipConnection;
+    public Button ipButton;
 
-    public void pressButton(ActionEvent event){
+    public void pressButton(ActionEvent event) {
         String user = "User: ";
         String text = textInput.getText();
         textInput.clear();
         chatWindow.appendText(user + text + "\n");
 
+    }
+
+    public void connectIP(ActionEvent actionEvent) throws IOException {
+        Stage stage = new Stage();
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        Pane root = fxmlLoader.load(getClass().getResource("ipConnection.fxml").openStream());
+        stage.setScene(new Scene(root, 240, 180));
+        stage.showAndWait();
 
     }
+
+    public void userName(ActionEvent actionEvent) throws IOException {
+        Stage stage = new Stage();
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        Pane root = fxmlLoader.load(getClass().getResource("userName.fxml").openStream());
+        stage.setScene(new Scene(root, 240, 180));
+        stage.showAndWait();
     }
+
+}
 
 
