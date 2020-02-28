319c48576e17458b8c437f45a3901cba4ab2d2f6
JoeA42 <36778393+JoeA42@users.noreply.github.com>
2020-02-28 11:33:23 -0600

    New Version!
    Changed the way windows are handled, now each window has its own controller, to avoid conflicts in a single controller file, and ease the process of editing.
    
    Also a general class called Misc was created, for code that can be reused in other windows.

---
+++ MainController.java
@@ -0,0 +1,58 @@
+package MAIN;
+
+import javafx.event.*;
+import javafx.scene.control.*;
+import java.io.*;
+
+public class MainController{
+
+    public RadioButton clientToggle = null;
+    public RadioButton serverToggle = null;
+    public TextField textInputClient = null;
+    public TextField userID = null;
+    public TextArea chatWindowClient = null;
+    public Button errorWindowOk = null;
+
+    public void optionSelect(ActionEvent event) {
+        if(clientToggle.isSelected()){
+            serverToggle.setSelected(false);
+            System.out.println("cliente seleccionado");
+        }
+        else if(serverToggle.isSelected()){
+            clientToggle.setSelected(false);
+            System.out.println("servidor seleccionado");
+        }
+
+    }
+
+    public String runButton(ActionEvent event) throws IOException {
+        String user = userID.getText();
+        System.out.println(user);
+        boolean button = false;
+        if(userID.getText().isEmpty()) {
+            ErrorController newWindow = new ErrorController();
+            System.out.println("ingrese un usuario valido");
+            newWindow.errorWindow();
+        }
+
+        else if(clientToggle.isSelected()) {
+            ClientController newWindow = new ClientController();
+            newWindow.clientChat();
+        }
+
+        else if(serverToggle.isSelected()){
+            ServerController newWindow = new ServerController();
+            newWindow.serverChat();
+        }
+
+        return null;
+    }
+    public String getUsername(){
+        String user = userID.getText();
+        userID.clear();
+        return user;
+
+    }
+}
+
+
